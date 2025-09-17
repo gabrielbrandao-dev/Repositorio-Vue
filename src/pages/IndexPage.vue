08a4af6e0cfe9de7ffa32c6cfd2bda24a71159c0
Gabriel Brandao <gabriel.brandao@agendaassessoria.com.br>
2025-09-17 11:40:30 -0400

    feat(projeto) adicionando projeto ao repositorio

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,38 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+
+  <div>
+
+    <q-row>
+      <h2>{{frases.titulo}}</h2>
+    </q-row>
+
+    <q-row>
+      <h5>{{frases.frase}}</h5>
+    </q-row>
+
+    <q-page class="flex flex-center">
+        <img
+        alt="Quasar logo"
+        src="~assets/quasar-logo-vertical.svg"
+        style="width: 300px; height: 300px"
+      >
+    </q-page>
+  </div>
+
 </template>
 
-<script setup>
-//
+<script>
+
+  export default {
+    data() {
+
+      return{
+        frases: {
+          frase: "A frase do dia é... Seja bondoso com todos, pois, oque você planta, no futuro há de ser colhido! ",
+          titulo: "Bela Frase"
+        }
+      }
+    }
+  }
+
 </script>
